55b226d55f406fbbb000dd3c67fc588d9219e4b9
unknown <yumiiiiiii>
2022-04-05 19:23:07 +0900

    first commit

---
+++ session2.py
@@ -0,0 +1,35 @@
+# 주석 하는 법 crtl+/
+
+# turn=10 
+# role="아기사자"
+# name="유미"
+
+# print(str(turn)+"기 "+role+" "+name)
+
+# print("안녕하세요, 제 이름은 {}이고, {}기 입니다.".format(name, turn))
+# print("안녕하세요, 제 이름은 %s이고, %d기 입니다." %(name, turn))
+
+# grade=int(input("점수를 입력하세요: "))
+# if(grade >= 90):
+#     print("A")
+# elif(grade >= 70):
+#     print("B")
+#     print("good!")
+# else:
+#     print("Fail")
+
+
+# a=int(input("숫자 a를 입력해주세요: "))
+# b=int(input("숫자 b를 입력해주세요: "))
+# c=int(input("a*b의 값은?: "))
+# if (c!=a*b):
+#     print("다시 한 번 생각해보세요.")
+# else:
+#     print("정답입니다!")
+
+
+
+color=["red","pink","yellow"]
+color.append("orange")
+color.insert(2,"purple")
+print(color)
